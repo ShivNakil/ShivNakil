e799484957e02c273e4baee83ce9674b5c886c9e
Shiv <tempacout@gmail.com>
2024-02-11 14:59:26 +0530

    pp

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      cursor: {
+        'custom': "url('./pp.jpg'), pointer",
+      }
+    },
   },
   plugins: [],
 }
